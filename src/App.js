943e4198e4ab949b48180aa64c43d94f8954dd8d
Ajit Kumar Gupta <ajit45288@gmail.com>
2022-05-03 15:04:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,45 @@
 import "./styles.css";
 import React, { useState } from "react";
 
-let bgcolorbg = "orange";
-let colorsText = "green";
-let data = "Project 123 started";
-
 function App() {
-  const [bgcolor, setBgcolor] = useState(bgcolorbg);
-  const [text, setText] = useState(data);
-  const [colors, setColors] = useState(colorsText);
-
-  const changebgColor = () => {
-    setBgcolor("cyan");
-    setText("aaaaaaasss");
-    setColors("red");
+  const [textData, setTextData] = useState("");
+  const [preData, setPreData] = useState("");
+  const clearData = () => {
+    setTextData("");
   };
-
-  const normalData = () => {
-    setBgcolor(bgcolorbg);
-    setText(data);
-    setColors(colorsText);
+  const handleData = (e) => {
+    setTextData(e.target.value);
   };
-
+  const previewData = () => {
+    setPreData(textData);
+  };
   return (
     <>
-      <div style={{ backgroundColor: bgcolor, color: colors }}>
-        <h1 className="text-center">{text}</h1>
+      <div className="container my-5">
+        <textarea
+          cols="30"
+          rows="10"
+          class="w-100"
+          onChange={handleData}
+          value={textData}
+        >
+          {textData}
+        </textarea>
+        <br />
+        <button className="btn btn-dark my-3" onClick={clearData}>
+          Clear Data
+        </button>
+        <p>{preData}</p>
+        <button className="btn btn-success" onClick={previewData}>
+          Preview Data
+        </button>
+        <p className="text-center text-danger">
+          {textData.length} Characters are present inside text area
+        </p>
+        <p className="text-center text-success">
+          {textData.split(" ").length} words are present inside
+        </p>
       </div>
-      <button
-        className="btn btn-dark"
-        onClick={changebgColor}
-        onDoubleClick={normalData}
-      >
-        Click me
-      </button>
     </>
   );
 }
